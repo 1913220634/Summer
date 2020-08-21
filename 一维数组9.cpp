a7929194525ae5abd19d1bb51264f8db0098bac6
王智力 <67962892+1913220634@users.noreply.github.com>
2020-08-21 16:08:16 +0800

    一维数组9
    已知数组声明为int[a]={0,10,20,30,40,50,60,70,80,90};,10个数组元素是按升序排列的。输入一个整数n，如果没有与n相等的数组元素，输出-1，否则，删除与n相等的数组元素，要求剩余的九个数组元素是按升序排列的，输出数组。

---
+++ 一维数组9.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int i,n,k,flag=0;
+	int a[10]={0,10,20,30,40,50,60,70,80,90};
+	printf("请输入一个整数:");
+	scanf("%d",&n);
+	for(i=0;i<10;i++)
+	{
+		if(a[i]==n)
+		{
+			k=i;
+			flag=1;
+		}
+	}
+    for(i=k;i<10;i++)
+			{
+				a[i]=a[i+1];
+				
+			}
+	if(flag==0)
+
+	{
+		printf("-1");
+	}
+	else
+	{
+		for(i=0;i<9;i++)
+			printf("%d  ",a[i]);
+	}
+	return 0;
+}
+
