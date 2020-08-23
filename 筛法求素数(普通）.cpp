5c9d001c5893845d5eb7a2fd40868e7a2d0cbedf
王智力 <67962892+1913220634@users.noreply.github.com>
2020-08-23 16:36:45 +0800

    筛法求素数

---
+++ 筛法求素数(普通）.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#define n 100
+int main()
+{
+	bool a[n + 1];
+	int i, j;
+	for (i = 2; i < n + 1; i++)
+	{
+		a[i] = true;
+	}
+	for (i = 2; i < n + 1; i++)
+	{
+		for (j = 2; j * i < n+1; j++)
+		{
+			a[i * j] = false;
+		}
+	}
+	for (i = 2; i < n + 1; i++)
+	{
+		if (a[i] == true)
+			printf("%d\n", i);
+	}
+	return 0;
+}
